a6bc779e6768bebf81982964b3e731968fe33e74
Merlinux-source <67962212+Merlinux-source@users.noreply.github.com>
2025-02-07 12:17:04 +0100

    Add JWT auth functionality (lesson id be93db0d-4c6d-49cf-b56d-ba22392eb160)
    Signed-off-by: Merlinux-source <67962212+Merlinux-source@users.noreply.github.com>

---
+++ internal/auth/MakeJWT.go
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 Merlinux-source
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	jwt.NewNumericDate(time.Now())
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{Issuer: "chirpy", IssuedAt: &jwt.NumericDate{time.Now().UTC()}, ExpiresAt: &jwt.NumericDate{time.Now().Add(expiresIn)}, Subject: userID.String()})
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+	return tokenString, err
+}
---
+++ internal/auth/ValidateJWT.go
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 Merlinux-source
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{Issuer: "chirpy"}, func(token *jwt.Token) (interface{}, error) { return []byte(tokenSecret), nil })
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
+		return uuid.Parse(claims.Subject)
+	}
+	return uuid.Nil, err
+}
